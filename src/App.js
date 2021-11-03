8d1bcdc9a9f30207663af3778a75eaef5126fd47
franckycodes <franckycodes@gmail.com>
2021-11-03 10:36:41 +0100

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const MYFORM=(
+	<form>
+<input type="text"></input>
+
+	</form>);
+const MYJSX=(<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+       
+        <p>hello francky</p>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >Learn React</a>
       </header>
-    </div>
-  );
+</div>);
+{/*some react comment*/}
+function App() {
+    return MYJSX;
 }
 
 export default App;
